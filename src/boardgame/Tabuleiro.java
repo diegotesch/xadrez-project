3a5053e6ab9d4399f04b11edc9cafaaacc7bda23
diegotesch <diego.tesch@gmail.com>
2019-12-25 11:42:51 -0300

    Tratando excecoes de posicao

--- src/boardgame/Tabuleiro.java
+++ src/boardgame/Tabuleiro.java
@@ -7,6 +7,9 @@ public class Tabuleiro {
     private Peca[][] pecas;
 
     public Tabuleiro(int linhas, int colunas) {
+        if(linhas < 1 || colunas < 1){
+            throw new TabuleiroException("Erro ao criar tabuleiro: É necessário que haja ao menos uma linha e uma coluna");
+        }
         this.linhas = linhas;
         this.colunas = colunas;
         pecas = new Peca[linhas][colunas];
@@ -16,28 +19,44 @@ public class Tabuleiro {
         return linhas;
     }
 
-    public void setLinhas(int linhas) {
-        this.linhas = linhas;
-    }
-
     public int getColunas() {
         return colunas;
     }
 
-    public void setColunas(int colunas) {
-        this.colunas = colunas;
-    }
-
     public Peca peca(int linha, int coluna){
+        if(!posicaoExiste(linha, coluna)){
+            throw new TabuleiroException("Posicao não existe no tabuleiro");
+        }
         return pecas[linha][coluna];
     }
 
     public Peca peca(Posicao posicao){
+        if(!posicaoExiste(posicao)){
+            throw new TabuleiroException("Posicao não existe no tabuleiro");
+        }
         return pecas[posicao.getLinha()][posicao.getColuna()];
     }
 
     public void colocarPeca(Peca peca, Posicao posicao){
+        if(existeUmaPeca(posicao)){
+            throw new TabuleiroException("Já existe uma peça na poisição: " + posicao);
+        }
         pecas[posicao.getLinha()][posicao.getColuna()] = peca;
         peca.posicao = posicao;
+    }
+
+    private boolean posicaoExiste(int linha, int coluna){
+        return linha >= 0 && linha < linhas && coluna >=0 && coluna < colunas;
+    }
+
+    public boolean posicaoExiste(Posicao posicao){
+        return posicaoExiste(posicao.getLinha(), posicao.getColuna());
+    }
+
+    public boolean existeUmaPeca(Posicao posicao){
+        if(!posicaoExiste(posicao)){
+            throw new TabuleiroException("Posicao não existe no tabuleiro");
+        }
+        return peca(posicao) != null;
     }
 }
