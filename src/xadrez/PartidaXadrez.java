f5a6dab2cf178594ee45cddba98b8ef58a84b64f
diegotesch <diego.tesch@gmail.com>
2019-12-29 12:29:19 -0300

    Implementado movimento special Castling

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -99,6 +99,24 @@ public class PartidaXadrez {
             pecasCapturadas.add(pecaCapturada);
         }
 
+//        #MOVIMENTO ESPECIAL CASTLING LADO DO REI
+        if(p instanceof Rei && destino.getColuna() == origem.getColuna() + 2){
+            Posicao origemT = new Posicao(origem.getLinha(), origem.getColuna() + 3);
+            Posicao destinoT = new Posicao(origem.getLinha(), origem.getColuna() + 1);
+            PecaXadrez torre = (PecaXadrez)tabuleiro.removePeca(origemT);
+            tabuleiro.colocarPeca(torre, destinoT);
+            torre.incrementaContadorMovimento();
+        }
+
+//        #MOVIMENTO ESPECIAL CASTLING LADO DA RAINHA
+        if(p instanceof Rei && destino.getColuna() == origem.getColuna() -2){
+            Posicao origemT = new Posicao(origem.getLinha(), origem.getColuna() - 4);
+            Posicao destinoT = new Posicao(origem.getLinha(), origem.getColuna() - 1);
+            PecaXadrez torre = (PecaXadrez)tabuleiro.removePeca(origemT);
+            tabuleiro.colocarPeca(torre, destinoT);
+            torre.incrementaContadorMovimento();
+        }
+
         return pecaCapturada;
     }
 
@@ -111,6 +129,24 @@ public class PartidaXadrez {
             tabuleiro.colocarPeca(pecaCapturada, destino);
             pecasCapturadas.remove(pecaCapturada);
             pecasNoTabuleiro.add(pecaCapturada);
+        }
+
+//        #MOVIMENTO ESPECIAL CASTLING LADO DO REI
+        if(p instanceof Rei && destino.getColuna() == origem.getColuna() + 2){
+            Posicao origemT = new Posicao(origem.getLinha(), origem.getColuna() + 3);
+            Posicao destinoT = new Posicao(origem.getLinha(), origem.getColuna() + 1);
+            PecaXadrez torre = (PecaXadrez)tabuleiro.removePeca(destinoT);
+            tabuleiro.colocarPeca(torre, origemT);
+            torre.decrementaContadorMovimento();
+        }
+
+//        #MOVIMENTO ESPECIAL CASTLING LADO DA RAINHA
+        if(p instanceof Rei && destino.getColuna() == origem.getColuna() -2){
+            Posicao origemT = new Posicao(origem.getLinha(), origem.getColuna() - 4);
+            Posicao destinoT = new Posicao(origem.getLinha(), origem.getColuna() - 1);
+            PecaXadrez torre = (PecaXadrez)tabuleiro.removePeca(destinoT);
+            tabuleiro.colocarPeca(torre, origemT);
+            torre.decrementaContadorMovimento();
         }
     }
 
@@ -198,7 +234,7 @@ public class PartidaXadrez {
         colocarNovaPeca('b', 1, new Cavalo(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('c', 1, new Bispo(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('d', 1, new Rainha(tabuleiro, Cor.BRANCO));
-        colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO, this));
         colocarNovaPeca('f', 1, new Bispo(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('g', 1, new Cavalo(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('h', 1, new Torre(tabuleiro, Cor.BRANCO));
@@ -216,7 +252,7 @@ public class PartidaXadrez {
         colocarNovaPeca('b', 8, new Cavalo(tabuleiro, Cor.PRETO));
         colocarNovaPeca('c', 8, new Bispo(tabuleiro, Cor.PRETO));
         colocarNovaPeca('d', 8, new Rainha(tabuleiro, Cor.PRETO));
-        colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO, this));
         colocarNovaPeca('f', 8, new Bispo(tabuleiro, Cor.PRETO));
         colocarNovaPeca('g', 8, new Cavalo(tabuleiro, Cor.PRETO));
         colocarNovaPeca('h', 8, new Torre(tabuleiro, Cor.PRETO));
